4339013b75f6c09c25189eb768cc60e436333015
Robert Hunnicutt <robhunn74@gmail.com>
2019-03-15 09:15:31 -0500

    add if statments

--- case-switch.sql
+++ case-switch.sql
@@ -36,4 +36,47 @@ SELECT title, stock_quantity,
         WHEN stock_quantity <= 100 THEN '**'
         ELSE '***'
     END AS STOCK
-FROM books; 
+FROM books;
+
+
+SELECT first_name AS 'First Name',
+    IFNULL(AVG(grade),0) AS Average,
+    CASE 
+WHEN AVG(grade) >= 75 THEN 'Passing' 
+WHEN AVG(grade) < 75 THEN 'Failing' 
+ELSE 'FLUNKY' 
+END AS Status
+FROM students
+    LEFT JOIN papers
+    ON students.id = papers.student_id
+GROUP BY first_name
+ORDER BY Average DESC;
+
+
+SELECT
+    first_name, last_name, COUNT(rating) AS Count,
+    IFNULL(MIN(rating),0) AS Min ,
+    IFNULL(MAX(rating),0) AS Max,
+    IFNULL(ROUND(AVG(rating),2),0) AS Avg,
+    CASE 
+WHEN COUNT(rating) > 0 THEN 'Active' 
+    ELSE 'Inactive' 
+        END AS STATUS
+FROM reviewers
+    LEFT JOIN reviews
+    ON reviewers.id = reviews.reviewer_id
+GROUP BY last_name,first_name
+ORDER BY last_name;
+
+------If Statment------
+SELECT
+    first_name, last_name, COUNT(rating) AS Count,
+    IFNULL(MIN(rating),0) AS Min ,
+    IFNULL(MAX(rating),0) AS Max,
+    IFNULL(ROUND(AVG(rating),2),0) AS Avg, 
+IF (COUNT(rating) >= 1, 'Active', 'Inactive') AS Status
+FROM reviewers 
+LEFT JOIN reviews 
+ON reviewers.id = reviews.reviewer_id
+GROUP BY last_name,first_name
+ORDER BY last_name;
