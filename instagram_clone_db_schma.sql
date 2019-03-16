55f08f6dae07872c592870a5b87bcf746fc9dcf7
Robert Hunnicutt <robhunn74@gmail.com>
2019-03-15 20:17:53 -0500

    schma db

--- instagram_clone_db_schma.sql
+++ instagram_clone_db_schma.sql
@@ -1,111 +1,72 @@
-mysql
->
-use instagram_db;
-Database changed
-mysql>
-CREATE TABLE users (
-    ->     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    ->     username VARCHAR
-(255) UNIQUE NOT NULL,
-    ->     created_at TIMESTAMP DEFAULT NOW
-()
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
+CREATE TABLE users
+(
+    id INTEGER
+    AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR
+    (255) UNIQUE NOT NULL,
+created_at TIMESTAMP DEFAULT NOW
+    ()
+);
 
-mysql>
-CREATE TABLE photos (
-    ->     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    ->     image_url VARCHAR
-(255) NOT NULL,
-    ->     user_id INTEGER NOT NULL,
-    ->     created_at TIMESTAMP DEFAULT NOW
-(),
-    ->     FOREIGN KEY
-(user_id) REFERENCES users
-(id)
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
+    CREATE TABLE photos
+    (
+        id INTEGER
+        AUTO_INCREMENT PRIMARY KEY,
+image_url VARCHAR
+        (255) NOT NULL,
+user_id INTEGER NOT NULL,
+created_at TIMESTAMP DEFAULT NOW
+        (),
+FOREIGN KEY
+        (user_id) REFERENCES users
+        (id)
+);
 
-mysql>
-CREATE TABLE comments (
-    ->     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    ->     comment_text VARCHAR
-(255) NOT NULL,
-    ->     photo_id INTEGER NOT NULL,
-    ->     user_id INTEGER NOT NULL,
-    ->     created_at TIMESTAMP DEFAULT NOW
-(),
-    ->     FOREIGN KEY
-(photo_id) REFERENCES photos
-(id),
-    ->     FOREIGN KEY
-(user_id) REFERENCES users
-(id)
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
+        CREATE TABLE comments
+        (
+            id INTEGER
+            AUTO_INCREMENT PRIMARY KEY,
+            comment_text VARCHAR
+            (255) NOT NULL,
+            photo_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW
+            (),
+            FOREIGN KEY
+            (photo_id) REFERENCES photos
+            (id),
+            FOREIGN KEY
+            (user_id) REFERENCES users
+            (id)
+);
 
-mysql>
-CREATE TABLE likes (
-    ->     user_id INTEGER NOT NULL,
-    ->     photo_id INTEGER NOT NULL,
-    ->     created_at TIMESTAMP DEFAULT NOW
-(),
-    ->     FOREIGN KEY
-(user_id) REFERENCES users
-(id),
-    ->     FOREIGN KEY
-(photo_id) REFERENCES photos
-(id),
-    ->     PRIMARY KEY
-(user_id, photo_id)
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
+            CREATE TABLE likes
+            (
+                user_id INTEGER NOT NULL,
+                photo_id INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW(),
+                FOREIGN KEY(user_id) REFERENCES users(id),
+                FOREIGN KEY(photo_id) REFERENCES photos(id),
+                PRIMARY KEY(user_id, photo_id)
+            );
 
-mysql>
-CREATE TABLE follows (
-    ->     follower_id INTEGER NOT NULL,
-    ->     followee_id INTEGER NOT NULL,
-    ->     created_at TIMESTAMP DEFAULT NOW
-(),
-    ->     FOREIGN KEY
-(follower_id) REFERENCES users
-(id),
-    ->     FOREIGN KEY
-(followee_id) REFERENCES users
-(id),
-    ->     PRIMARY KEY
-(follower_id, followee_id)
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
+            CREATE TABLE follows
+            (
+                follower_id INTEGER NOT NULL,
+                followee_id INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW(),
+                FOREIGN KEY(follower_id) REFERENCES users(id),
+                FOREIGN KEY(followee_id) REFERENCES users(id),
+                PRIMARY KEY(follower_id, followee_id)
+            );
 
-mysql>
-CREATE TABLE tags (
-    ->   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    ->   tag_name VARCHAR
-(255) UNIQUE,
-    ->   created_at TIMESTAMP DEFAULT NOW
-()
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
+            CREATE TABLE tags
+            (
+                id INTEGER
+                AUTO_INCREMENT PRIMARY KEY,
+                tag_name VARCHAR
+                (255) UNIQUE,
+                created_at TIMESTAMP DEFAULT NOW
+                ()
+);
 
-mysql>
-CREATE TABLE photo_tags (
-    ->     photo_id INTEGER NOT NULL,
-    ->     tag_id INTEGER NOT NULL,
-    ->     FOREIGN KEY
-(photo_id) REFERENCES photos
-(id),
-    ->     FOREIGN KEY
-(tag_id) REFERENCES tags
-(id),
-    ->     PRIMARY KEY
-(photo_id, tag_id)
-    -> );
-Query OK, 0 rows affected
-(0.01 sec)
